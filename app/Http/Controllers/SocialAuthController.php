82bb8cf496021882d6538b1f4d4a387be637f2ba
Clay Ewing <c.ewing@miami.edu>
2018-10-12 11:13:34 -0400

    adding social logins

---
+++ app/Http/Controllers/SocialAuthController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SocialAuthController extends Controller
+{
+    //
+	public function redirect($service) {
+        return Socialite::driver ( $service )->redirect ();
+    }
+
+	public function callback($service) {
+        $user = Socialite::with ( $service )->user ();
+        return view ( 'home' )->withDetails ( $user )->withService ( $service );
+    }    
+}
