924784bb86c522802223f871ef9b5b711c8218f5
Clay Ewing <c.ewing@miami.edu>
2018-10-15 10:32:11 -0400

    add script section

--- resources/views/story.blade.php
+++ resources/views/story.blade.php
@@ -8,6 +8,15 @@
 @endsection
 
 @section('content')
+
+<div class="row">
+  <div class="col-sm-12">
+    <h1>{{ $story->title}}</h1>
+    <h2>{{ $story->authors}}</h2>
+      <div id="squiffy"></div>
+  </div>
+</div>
+@section('scripts')
         <script src="/js/stories/{{ $story->squiffy }}.js" charset="UTF-8"></script>
 
         <script>
@@ -26,11 +35,4 @@
             });
         </script>
 
-<div class="row">
-  <div class="col-sm-12">
-    <h1>{{ $story->title}}</h1>
-    <h2>{{ $story->authors}}</h2>
-      <div id="squiffy"></div>
-  </div>
-</div>
 @endsection
