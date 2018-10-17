f464e19507e5c9aee9165c0d869f780029ddcf54
Clay Ewing <c.ewing@miami.edu>
2018-10-16 21:36:20 -0400

    update master layout for home link

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,7 +23,7 @@
 
         @section('topnav')
             <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
-              <a class="navbar-brand" href="#">Living Jim Crow</a>
+              <a class="navbar-brand" href="{{ url('/')}}">Living Jim Crow</a>
               <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
               </button>
