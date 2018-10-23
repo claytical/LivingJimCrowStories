6d0cc831e40226234d1ac0c0137385c15dd5a416
Clay Ewing <c.ewing@miami.edu>
2018-10-23 17:41:10 -0400

    fix pivot class

--- app/PlayerVaultItems.php
+++ app/PlayerVaultItems.php
@@ -5,7 +5,7 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\Pivot;
 
-class PlayerVaultItems extends Pivot;
+class PlayerVaultItems extends Pivot
 {
     //
 	public function user()
