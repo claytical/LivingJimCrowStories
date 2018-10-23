f21064cb0b5a75cf71514ac92b92395f3dc930d2
Clay Ewing <c.ewing@miami.edu>
2018-10-23 11:36:32 -0400

    display exception

--- app/Http/Controllers/SocialAuthController.php
+++ app/Http/Controllers/SocialAuthController.php
@@ -20,7 +20,7 @@ class SocialAuthController extends Controller
             $user = Socialite::with($service)->user();
         }
         catch (\Exception $e) {
-            return redirect('/login');
+            return redirect('/login')->with('success', $e->getMessage());;
         }
 
         $authUser = $accountService->findOrCreate($user, $service);
